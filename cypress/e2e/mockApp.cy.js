e2ca0ef63128a8afe75ff856b537b67c00479011
Gamboa Carlos <cgamboa@atsistemas.com>
2022-10-07 10:29:12 +0200

    update test

--- cypress/e2e/mockApp.cy.js
+++ cypress/e2e/mockApp.cy.js
@@ -2,22 +2,14 @@
 describe('test app', () => {
     beforeEach(() => {
         cy.viewport(1280, 768)
-        cy.visit('http://localhost:3000')
-    })
-
-    it('should authenticate & display clubs first page', () => {
-        cy.get('#email').type('fake.user@fake.com')
-        cy.get('#password').type('123')
-        
+        cy.visit('http://localhost:3000')        
         // Mock auth ====================================================================
         cy.fixture('mockAuth').then(function(mockAuth){
             cy.intercept('POST', 'http://localhost:4000/login', {
                 status: 200,
                 body: mockAuth
             }).as('mock login')
-            cy.get('button').click()
         })
-
         // Mock clubs ====================================================================
         cy.fixture('mockClubs').then(function(mockClubs){
             cy.intercept('GET', 'http://localhost:4000/api/clubs?offset=0&limit=6', {
@@ -25,5 +17,12 @@ describe('test app', () => {
                 body: mockClubs
             }).as('mock clubs')
         })
+    })
+
+    it('should authenticate & display clubs first page', () => {
+        cy.get('#email').type('fake.user@fake.com')
+        cy.get('#password').type('123')
+        cy.get('button').click()
+        cy.getByTestId('club-card').should('have.length', 6)
     })
 })
